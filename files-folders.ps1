21cfc2f938c386c3355f5d398f434470cb256dc4
vignesh sweekaran <vigneshsweekaran@gmail.com>
2022-08-21 22:38:51 +0530

    Adding powershell script

---
+++ files-folders.ps1
@@ -0,0 +1,10 @@
+﻿$PSVersionTable
+
+# To create folder
+New-Item -Path 'D:\test\Vignesh Folder' -ItemType Directory
+
+# To create file
+New-Item -Path 'D:\test\hello.txt' -ItemType File
+
+# To Copy Folder
+Copy-Item 'D:\test\Vignesh Folder' 'E:\test\'
